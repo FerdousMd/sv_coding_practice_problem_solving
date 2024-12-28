60f70d19f4d7f43cde0eac497242bd0eade9ba74
sadman <ferdousmdsadman@outlook.com>
2024-12-28 07:39:21 +0200

    Randomized even number between 50 & 100 done

---
+++ rand_even.sv
@@ -0,0 +1,34 @@
+//Write a constraint to generate a random even number between 50 and 100
+
+class rand_even;
+  
+  randc int even;
+  
+  //rand int even;
+  
+  constraint c1 {even%2 == 0;}
+  
+  constraint c2 {even inside {[50:100]};}
+  
+  function void post_randomize();
+    
+    $display(even);
+    
+  endfunction: post_randomize
+  
+endclass: rand_even
+
+
+rand_even re;
+
+
+module even;
+  
+  initial begin
+    
+    re = new();
+    repeat (10) assert(re.randomize());
+    
+  end
+  
+endmodule: even
